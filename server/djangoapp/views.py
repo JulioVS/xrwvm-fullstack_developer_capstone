5774c64c9feb97f0357edfa8819312c83b4c310e
JulioVS <julioer@me.com>
2024-09-18 19:13:32 -0400

    Fix even more syntax issues

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -32,7 +32,7 @@ def get_cars(request):
     cars = []
     for car_model in car_models:
         cars.append({"CarModel": car_model.name,
-        "CarMake": car_model.car_make.name})
+            "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
@@ -80,12 +80,14 @@ def registration(request):
     except Exception as err:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
+        logger.debug(err)
 
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
         user = User.objects.create_user(username=username,
-        first_name=first_name, last_name=last_name, password=password, email=email)
+            first_name=first_name, last_name=last_name, password=password,
+            email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
@@ -95,10 +97,6 @@ def registration(request):
         return JsonResponse(data)
 
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if (state == "All"):
         endpoint = "/fetchDealers"
@@ -148,7 +146,8 @@ def add_review(request):
             post_review(data)
             return JsonResponse({"status": 200})
         except Exception as err:
+            logger.debug(err)
             return JsonResponse({"status": 401,
-            "message": "Error in posting review"})
+                "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
